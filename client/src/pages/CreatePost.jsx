6a7515396cf747bcc9ace24efaa694f753d776c2
claudiaaziz <claudiahannahh@gmail.com>
2024-06-26 00:33:01 -0400

    Add post form and style it and posts persisting to db

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import Axios from 'axios'
+
 const CreatePost = () => {
-    return <div className="create-post">CreatePost</div>;
+    const [username, setUsername] = useState('');
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const createPost = () => {
+        Axios.post('http://localhost:5010/api/create', {username, title, body})
+    }
+
+    return (
+        <div className='create-post'>
+            <form action=''>
+                <label htmlFor='username'>Username:</label>
+                <input
+                    type='text'
+                    id='username'
+                    placeholder='Your username'
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label htmlFor='title'>Title:</label>
+                <input
+                    type='text'
+                    id='title'
+                    placeholder='Title of your post'
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label htmlFor='body'>Body:</label>
+                <textarea
+                    name=''
+                    id='body'
+                    placeholder='Your post content'
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <button onClick={createPost}>Create Post</button>
+            </form>
+        </div>
+    );
 };
 
 export default CreatePost;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./config/db');
 const app = express();
 
 const PORT = 5010;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {});
 
-app.post('api/create', (req, res) => {
+app.post('/api/create', (req, res) => {
+    const username = req.body.username;
+    const title = req.body.title;
+    const body = req.body.body;
+
     db.query(
-        "INSERT INTO Posts (title, body, username) VALUES ()", (err, result) => {
-            if (err) console.log(err)
-            else console.log(result)
+        'INSERT INTO Posts (username, title, body) VALUES (?,?,?)',
+        [username, title, body],
+        (err, result) => {
+            if (err) console.log(err);
+            else console.log(result);
         }
     );
 });
